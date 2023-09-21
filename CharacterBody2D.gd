40214196d55a339d5987b3dd8105abb6c9a236c8
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2023-09-21 13:47:33 -0400

    Initial commit

---
+++ CharacterBody2D.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var speed = 300.0
+var jump_speed = 400.0
+
+# Get the gravity from the project settings so you can sync with rigid body nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_pressed("saut"):
+		# Apply an upward force to make the character jump
+		velocity.y = -jump_speed
+	else:
+		velocity.y += gravity * delta  # Apply gravity to simulate falling
+
+
+	# Get the input direction.
+	var direction = Input.get_axis("mouvement-gauche", "mouvement-droit")
+	velocity.x = direction * speed
+
+	move_and_slide()
